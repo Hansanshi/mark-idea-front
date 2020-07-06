de772690ada52913929c8f6c7baba2d6084898ad
hansanshi <hansanshi@outlook.com>
2020-07-06 23:12:59 +0800

    bug: fix css

---
+++ src/global.js
@@ -0,0 +1,5 @@
+const HOST_URL = 'http://127.0.0.1:8090/api'
+
+export default{
+    HOST_URL
+}
--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import contentmenu from 'v-contextmenu'
+import 'v-contextmenu/dist/index.css'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.use(contentmenu)
 
 new Vue({
   router,
